384f2e81be29367b0327072a8156c4515c6aa732
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-03-22 20:26:40 -0600

    added core data, an alert view, and a pull-down refresh

--- SimpleView/SimpleView/TableViewController.swift
+++ SimpleView/SimpleView/TableViewController.swift
@@ -7,38 +7,46 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
-
-    var arrayOfTasks = [Task]()
+    
+    //MARK: -Properties
+    
+    var arrayOfTasks = [NSManagedObject]()
+    
+    var moc = DataController().managedObjectContext
+    
+    var nameString = ""
+    
+    var dateFormatter = NSDateFormatter()
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "handleRefresh:", forControlEvents: UIControlEvents.ValueChanged)
+        
+        return refreshControl
+    }()
     
     @IBOutlet weak var tableView: UITableView!
     
-
+    @IBAction func addTask(sender: UIBarButtonItem) {
+        
+        self.showAlert("Add a task", message: "Type task name here")
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-    }
-    
-    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
         
-        if segue.identifier == "unwindSegue" {
-            
-            let taskViewController = segue.sourceViewController as! TaskViewController
-            
-            let t = Task()
-            
-            if let name = taskViewController.taskTextField.text {
-                t.name = name
-            }
-            
-            self.arrayOfTasks.append(t)
-            
-            self.tableView.reloadData()
-        }
+        self.tableView.addSubview(self.refreshControl)
+        
+        dateFormatter.dateFormat = "HH:mm:ss"
+        
+        fetchTasks()
         
     }
+    //MARK: - Table View Functions
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
@@ -46,15 +54,215 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
         
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("taskCell")!
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         let t = arrayOfTasks[indexPath.row]
         
-        cell.textLabel?.text = t.name
+        if let didSelectBool = t.valueForKey("isCompleted") as? Bool {
+            if didSelectBool == true {
+                t.setValue(false, forKey: "isCompleted")
+            } else if didSelectBool == false {
+                t.setValue(true, forKey: "isCompleted")
+            }
+        }
         
+        self.tableView.reloadData()
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var date = NSDate()
+        
+        var isCompleted: Bool = false
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("taskCell", forIndexPath: indexPath) as! TaskTableViewCell
+        
+        let task = self.arrayOfTasks[indexPath.row]
+        
+        if let complete = task.valueForKey("isCompleted") as? Bool {
+            isCompleted = complete
+        }
+        
+        if let name = task.valueForKey("name") as? String {
+            cell.taskNameLabel.text = name
+        }
+        if let dateVariable = task.valueForKey("created") as? NSDate {
+            let createdDate = dateFormatter.stringFromDate(dateVariable)
+            print(createdDate)
+            cell.dateLabel.text = createdDate
+            
+            date = dateVariable
+        }
+        
+        if isCompleted == true {
+            
+            cell.accessoryType = .Checkmark
+            cell.backgroundColor = UIColor .lightGrayColor()
+            cell.taskNameLabel.textColor = UIColor .whiteColor()
+            
+        }
+        if isCompleted == false {
+            
+            cell.accessoryType = .None
+            cell.taskNameLabel.textColor = UIColor .blackColor()
+            
+            if timeElapsed(date) < 120 {
+                cell.backgroundColor = UIColor .greenColor()
+            } else if timeElapsed(date) < 300 {
+                cell.backgroundColor = UIColor .yellowColor()
+            } else {
+                cell.backgroundColor = UIColor .redColor()
+            }
+        }
         return cell
     }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            let taskToDelete = arrayOfTasks[indexPath.row]
+            moc.deleteObject(taskToDelete)
+            
+            self.fetchTasks()
+            self.saveAndLoad()
+            
+        }
+    }
+    
+    //MARK: -Alert View
+    
+    func showAlert(title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default) {
+            (alertAction) -> Void in
+            
+            if let textField = alertController.textFields?.first,
+                let name = textField.text {
+                    
+                    self.saveTask(name)
+                    self.tableView.reloadData()
+            }
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) {
+            (alertAction) -> Void in
+            
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler {
+            
+            (textField) -> Void in
+        }
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil )
+    }
+    
+    //MARK: -CoreData Save
+    
+    func saveTask(name: String) {
+        
+        if let taskEntity = NSEntityDescription.entityForName("Task", inManagedObjectContext: self.moc) {
+            
+            let task = NSManagedObject(entity: taskEntity, insertIntoManagedObjectContext: self.moc)
+            
+            let date = NSDate()
+            
+            task.setValue(name, forKey: "name")
+            
+            task.setValue(date, forKey: "created")
+            
+            task.setValue(false, forKey: "isCompleted")
+            
+            do {
+                
+                try self.moc.save()
+                print("I saved task \(name)")
+                
+                self.arrayOfTasks.append(task)
+                
+            } catch {
+                
+                print("I couldn't save task \(name)")
+            }
+            
+        }
+        
+    }
+    
+    func saveAndLoad() {
+
+        do {
+            
+            try self.moc.save()
+            
+            self.tableView.reloadData()
+            
+        } catch {
+            
+            print("saveAndLoad doesn't work")
+        }
+
+    }
+    
+    func fetchTasks() {
+        
+        //Step 1
+        let fetchRequest = NSFetchRequest(entityName: "Task")
+        
+        //Step 2
+        let sortDescriptor = NSSortDescriptor(key: "created", ascending: false)
+        
+        //Step 3 Add sort descripto
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            
+            if let results = try self.moc.executeFetchRequest(fetchRequest) as? [NSManagedObject] {
+                
+                self.arrayOfTasks = results
+                self.tableView.reloadData()
+                
+            }
+            
+        } catch {
+            
+            print("Error: couldn't fetch tasks")
+        }
+        
+    }
+    
+    func timeElapsed(startTime: NSDate) -> NSTimeInterval {
+        
+        let endTime = NSDate()
+        
+        let formatter = NSNumberFormatter()
+        
+        formatter.maximumFractionDigits = 0
+        
+        let timeInterval: Double = endTime.timeIntervalSinceDate(startTime);
+        
+        let numberString = formatter.stringFromNumber(timeInterval)
+        
+        print(numberString)
+        
+        return timeInterval
+    }
+    
+    func handleRefresh(refreshControl: UIRefreshControl) {
+        
+        fetchTasks()
+        
+        self.tableView.reloadData()
+        
+        refreshControl.endRefreshing()
+        
+    }
+    
 }
 
